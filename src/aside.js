fe949440dadcd96163655189dac841a30ec70d52
raul151999 <166523842+raul151999@users.noreply.github.com>
2024-04-24 04:47:15 +0000

    Agregando archivo aside.js

---
+++ src/aside.js
@@ -0,0 +1,25 @@
+const Aside = () => {
+    return( 
+      <aside>
+        <form>
+          <div className="mb-5 py-5">
+            <label htmlFor="exampleInputEmail1" className="form-label ">Email</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <div id="emailHelp" className="form-text">No se compartira su correo electrónico</div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" />
+          </div>
+          <div className="mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+            <label className="form-check-label" htmlFor="exampleCheck1">Echar un vistazo</label>
+          </div>
+          <button type="submit" className="btn btn-primary">Entregar</button>
+        </form>
+      </aside>
+    );
+  }
+  
+  export default Aside;
+  
